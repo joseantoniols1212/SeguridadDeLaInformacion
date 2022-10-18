b136329206a052875dc343bbafcac38e1f2fa2de
joseantoniols1212 <joseantonioluquesalguero1212@gmail.com>
2022-10-18 13:08:16 +0200

    Practica3

---
+++ Practica3/ca.py
@@ -0,0 +1,8 @@
+from CodigoApendiceA import *
+
+# Apartado a
+key = crear_RSAKey()
+guardar_RSAKey_Privada("privada_ca", key, "password1234")
+guardar_RSAKey_Publica("publica_ca", key)
+
+# Apartado b
